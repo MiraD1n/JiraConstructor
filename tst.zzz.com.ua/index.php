5034c22fbc91f2068ef362c44d30f0da714ae563
Полищук Максим Юрьевич <mpolishchuk@just2trade.online>
2019-10-16 10:51:30 +0300

    add via RFC2 curl to JIRA

--- tst.zzz.com.ua/index.php
+++ tst.zzz.com.ua/index.php
@@ -1,3 +1,11 @@
 <?php include_once "header.php";?>
-Test
+<form action="https://sd.finam.ru/issues/">
+<p><b>Type:</b><Br>
+<input type="radio" name="jql" value="Incident">Incident</br>
+<input type="radio" name="jql" value="cf[21001] = IT-22852">RFC2</br>
+<input type="radio" name="jql" value="RFC">RFC</br>
+<input type="radio" name="jql" value="Release">Release</br>
+</p>
+<input type="submit">
+</form>
 <?php include_once "footer.php";?>
