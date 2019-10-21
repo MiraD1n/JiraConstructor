179d1560a413c24db8a6f62cac8779c689ae516f
MiraD1n <mupolischuk@gmail.com>
2019-10-21 19:49:08 +0300

    add some

--- tst.zzz.com.ua/backup.php
+++ tst.zzz.com.ua/backup.php
@@ -1,5 +1,7 @@
+<?php
 echo '<form enctype="multipart/form-data" name="upload_bage" id="upload_bage" action="upload.php" method="POST">
         <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
         <input name="userfile" type="file" />
         <input type="submit" value="Отправить файл" />
       </form>';
+?>
