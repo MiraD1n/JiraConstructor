9c9b509b54ab3a3d3e29d3f0205e0098caba31de
Полищук Максим Юрьевич <mpolishchuk@just2trade.online>
2019-10-23 10:26:22 +0300

    cosmetics work

--- tst.zzz.com.ua/index.php
+++ tst.zzz.com.ua/index.php
@@ -1,32 +1,5 @@
 <?php include_once "header.php";?>
-<!--
-<form action="https://sd.finam.ru/issues/">
-<p><b>Type:</b><Br>
-<input type="radio" name="jql" value="Incident">Incident</br>
-<input type="radio" name="jql" value="cf[21001] = IT-22852">RFC2</br>
-<input type="radio" name="jql" value="RFC">RFC</br>
-<input type="radio" name="jql" value="Release">Release</br>
-</p>
-<input type="submit">
-</form>
-<form action="">
-<p><b>Последние инциденты:</b><Br>
-<input type="hidden" type="text" name="jql" value="cf[21001]" /><br />
-<input type="text" name="text" value="777"></br>
-</p>
-<input type="submit">
-</form>
-<p>*открытые алерты по имени сервиса <p>
-<p>*открытые и переданные по имени сервиса <p>
-<p>*алерты за промежуток времени <p>
--->
-
-
-<!--  <form action = 'https://sd.finam.ru/issues/?jql=status%20was%20Done%20by%20mpolischuk%20before%%20after%<?php $_POST['date_from'];?>' method="post"> -->
-  <form action = "make_url.php" method="post">
-<!--   work but comment       <label for="date_from">От: </label> -->
-<!-- <input type="datetime-local" id="date_from" name="date_from"/> <input type="datetime-local" id="date_to" name="date_to"/><br />   -->
-
+<form action = "make_url.php" method="post">
 Hours from: <select id="hour_from" name="hour_from">
   <option>00</option>
   <option>01</option>
