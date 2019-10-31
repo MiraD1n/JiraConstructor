06262fe0c39858da9d85bcc922def71da15d7a8f
Полищук Максим Юрьевич <mpolishchuk@just2trade.online>
2019-10-31 10:52:02 +0300

    fix rfc url

--- jql.zzz.com.ua/rfc_finder.php
+++ jql.zzz.com.ua/rfc_finder.php
@@ -4,6 +4,6 @@ $object = $_POST['object'];
 
 //echo $object;
 
-header("Location: https://sd.finam.ru/issues/?filter=44851&jql=text%20~%20$object%20AND%20type%20%3D%20RFC%20ORDER%20BY%20key%20DESC");
+header("Location: https://sd.finam.ru/issues/?jql=text%20~%20$object%20AND%20type%20%3D%20RFC%20ORDER%20BY%20key%20DESC");
 
 ?>
